994c1cc4d58e4646a297a55f72b4f89c9e9c9de2
Jalaj Purohit <Jalajpurohit09@gmail.com>
2021-05-30 01:34:59 +0530

    Update check_permutation.py

--- CTCI/Chapter 7 - Interview Questions/1. Array and Strings/check_permutation.py
+++ CTCI/Chapter 7 - Interview Questions/1. Array and Strings/check_permutation.py
@@ -4,43 +4,72 @@
 # Hint 3: Could a hash table be useful?
 # Hint 4: Two strings that are permutations should have the same characters, but in different orders. Can you make orders the same?
 
+import unittest
+from collections import Counter
 
-# Solution 1: With use of list, a one pass solution
+# Solution 1: With use of list
 # Time Complexity: O(N)
-NO_OF_CHARS = 256
-def check_permutations_with_list(str1, str2):
-	count = [0 for i in range(NO_OF_CHARS)]
-	ind = 0
-	n1 = len(str1)
-	n2 = len(str2)
-	if n1 != n2:
-	    return False
-	
-	while ind < n1:
-	    count[ord(str1[ind])] += 1
-	    count[ord(str2[ind])] -= 1
-	    ind += 1
+def check_permutation_by_count(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    counter = [0] * 256
+    for c in str1:
+        counter[ord(c)] += 1
+    for c in str2:
+        if counter[ord(c)] == 0:
+            return False
+        counter[ord(c)] -= 1
+    return True
 
-	for i in range(NO_OF_CHARS):
-		if count[i]:
-		    return False;
-	return True;
 
 # Solution 2: With use of sorting
 # Time Complexity: O(N**2) as QuickSort is being used in sorting. If use MergeSort than we can reduce time complexity to O(NlogN)
-def check_permutations_with_sorting(s1, s2):
-    l1 = len(str1)
-    l2 = len(str2)
-    
-    if (n1 != n2):
+def check_permutation_by_sort(s1, s2):
+    if len(s1) != len(s2):
         return False
-
-    s1 = sorted(s1)
-    s1 = " ".join(s1)
-    s2 = sorted(s2)
-    s2 = " ".join(s2)
- 
-    for i in range(0, n1, 1):
-        if (str1[i] != str2[i]):
+    s1, s2 = sorted(s1), sorted(s2)
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
             return False
     return True
+
+
+# Solution 3: Pythonic Solution
+def check_permutation_pythonic(str1, str2):
+    if len(str1) != len(str2):
+        return False
+
+    return Counter(str1) == Counter(str2)
+
+
+class Test(unittest.TestCase):
+    # str1, str2, is_permutation
+    test_cases = (
+        ("dog", "god", True),
+        ("abcd", "bacd", True),
+        ("3563476", "7334566", True),
+        ("wef34f", "wffe34", True),
+        ("dogx", "godz", False),
+        ("abcd", "d2cba", False),
+        ("2354", "1234", False),
+        ("dcw4f", "dcw5f", False),
+        ("DOG", "dog", False),
+        ("dog ", "dog", False),
+        ("aaab", "bbba", False),
+    )
+
+    testable_functions = [
+        check_permutation_by_sort,
+        check_permutation_by_count,
+        check_permutation_pythonic,
+    ]
+
+    def test_cp(self):
+        # true check
+        for check_permutation in self.testable_functions:
+            for str1, str2, expected in self.test_cases:
+                assert check_permutation(str1, str2) == expected
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
