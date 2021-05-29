481b7a468459ca0cc5e7435d0d284457f1d3237b
Jalaj Purohit <Jalajpurohit09@gmail.com>
2021-05-30 01:47:06 +0530

    Create 04_palindrome_permutation.py

---
+++ CTCI/Chapter 7 - Interview Questions/1. Array and Strings/04_palindrome_permutation.py
@@ -0,0 +1,14 @@
+'''
+Problem Statement:
+Palindrome Permutation - Given a string, write a function to check if it is a permutation of a palindrome.
+A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
+The palindrome does not need to be limited to just dictionary words.
+Example:
+Input: Tact Coa
+Ouput: True (permutations: 'taco cat', 'atco cta', etc.)
+
+Hint 1: You do not have to - and should not - generate all permutations. This would be very inefficient.
+Hint 2: What characteristics would a string that is a permutation of a palindrome have?
+Hint 3: Have you tried a hash table? You should be able to get this down to O(N) time.
+Hint 4: Can you reduce the space uasge by using a bit vector?
+'''
