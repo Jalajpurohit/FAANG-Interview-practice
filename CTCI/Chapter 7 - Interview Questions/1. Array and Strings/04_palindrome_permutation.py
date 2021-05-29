95f0878471bb9d1307edb9cf0866ee90e2f00107
Jalaj Purohit <Jalajpurohit09@gmail.com>
2021-05-30 03:10:41 +0530

    Update 04_palindrome_permutation.py

--- CTCI/Chapter 7 - Interview Questions/1. Array and Strings/04_palindrome_permutation.py
+++ CTCI/Chapter 7 - Interview Questions/1. Array and Strings/04_palindrome_permutation.py
@@ -3,6 +3,7 @@ Problem Statement:
 Palindrome Permutation - Given a string, write a function to check if it is a permutation of a palindrome.
 A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.
 The palindrome does not need to be limited to just dictionary words.
+
 Example:
 Input: Tact Coa
 Ouput: True (permutations: 'taco cat', 'atco cta', etc.)
