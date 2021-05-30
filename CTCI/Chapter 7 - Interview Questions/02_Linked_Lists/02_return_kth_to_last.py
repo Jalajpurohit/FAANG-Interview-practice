e74ac1d0ffadfe51613dcd00febdb6a5162d0def
Jalaj Purohit <Jalajpurohit09@gmail.com>
2021-05-31 01:04:39 +0530

    Update 02_return_kth_to_last.py

--- CTCI/Chapter 7 - Interview Questions/02_Linked_Lists/02_return_kth_to_last.py
+++ CTCI/Chapter 7 - Interview Questions/02_Linked_Lists/02_return_kth_to_last.py
@@ -2,12 +2,14 @@
 Problem Statement:
 Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
 
-8 Hint 1:
-25 Hint 2:
+Hint 1: What if you knew the linked list size? What is the difference between finding the Kth-to-last element and
+        finding the Xth element?
+Hint 2: If you don't know the linked list size, can you compute it? How does it impace the runtime?
 Hint 3: Try implementing it recursively. If you could find the (K-1)the to last element, can you find the Kth element?
 Hint 4: You might find it useful to return multiple values. Some languages don't directly support this, but there are
         workarounds in essentially any language. What are some of those workarounds?
-126 Hint 5:
+Hint 5: Can you do it iteratively? Imagine if you had two pointers pointing to the adjacent nodes and they were moving
+        at the same speed through the linked list. When one hits the end of the linked list, where will the other be?
 '''
 
 from 02_Linked_Lists.linked_list import LinkedList
