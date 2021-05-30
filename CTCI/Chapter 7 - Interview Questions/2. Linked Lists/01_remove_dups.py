2d4fe589845c487ab1ffd8fe93100046540da69b
Jalaj Purohit <Jalajpurohit09@gmail.com>
2021-05-30 19:07:34 +0530

    Create 01_remove_dups.py

---
+++ CTCI/Chapter 7 - Interview Questions/2. Linked Lists/01_remove_dups.py
@@ -0,0 +1,9 @@
+'''
+Problem Statement:
+Remove Dups: Write code to remove duplicates from an unsorted linked list.
+
+Follow up: How would you solve this problem if a temporary buffer is not allowed?
+
+Hint 1: Have you tried a hash table? You should be able to do this in a single pass of the linked list.
+Hint 2: Without extra space, you'll need O(N**2) time. Try using two pointers, where the second one searches ahead of the first one.
+'''
